36491387b4442d508467b64de93b7df3f55423c5
OGVibes <shi.lee@my.liu.edu>
2026-04-29 11:02:35 -0400

    Edited upload image body, deleted javascript folder, and added more labelling in the html file.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
+import OpenAi from 'openai';
+const dropArea = getElementById("drop-area");
+//considering editing this upload function but it works so maybe not
 var loadFile = function (event) {
     var image = document.getElementById("output");
     image.src = URL.createObjectURL(event.target.files[0]);
 };
+function counter(){
+    //include openai calls to check image upload
+    count = count + 1;
+    if (count >= 3){
+        document.getElementById("first").innerText = "You got submitted than 3 incorrect images.";
+    }
+    return count;
+}
