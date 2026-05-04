3f888bcaa73c3203e7e6d53d4993778382d0ab53
SL <hellopeople11220@gmail.com>
2026-05-03 22:49:02 -0400

    Edited how the api's response interacts with other functions

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
-/*import OpenAI from 'openai';
+import OpenAI from 'openai';
 import fs from 'fs';
 
 const client = new OpenAI();
-*/
+
 const inputFile = document.getElementById("input-file");
 const dropArea = document.getElementById("drop-area");
 const imageView = document.getElementById("img-view");
-/*const base64Image = fs.readFileSync(inputFile, "base64");
+const base64Image = fs.readFileSync(inputFile, "base64");
+let b64imgLink = URL.createObjectURL(base64Image);
+let count = 0;
 
 const response = await client.responses.create({
     model: "gpt-4.1-mini",
@@ -19,21 +21,21 @@ const response = await client.responses.create({
                     text: "Does this image have a hot dog in it? Answer in either yes or no." },
                 {
                     type: "input_image",
-                    image_url: `data:image/jpeg;base64,${base64Image}`,
+                    image_url: `url(${b64imgLink})`,
                 },
             ],
         },
     ],
 });
 
-console.log(response.output_text);*/
+let responseOutput = response.output_text;
 
 inputFile.addEventListener("change", uploadImage);
 
 function uploadImage(){
     let imgLink = URL.createObjectURL(inputFile.files[0]);
     imageView.style.backgroundImage = `url(${imgLink})`;
-    imageView.textContent = "";
+    imageView.textContent = counter();
     imageView.style.border = 0;
 }
 
@@ -47,8 +49,10 @@ dropArea.addEventListener("drop", function(e){
 });
 
 function counter() {
-    //include openai calls to check image upload
-    count = count + 1;
+    //very basic checker 
+    if (responseOutput.toLowerCase().indexOf("no") != -1){
+        count = count + 1;
+    }
     if (count >= 3) {
         alert("You submitted at least more than 3 incorrect images.");
     }
